eaf8a7d65de216bf274b21f23876790b64d744f8
RyamL1221 <ryaml1221@gmail.com>
2023-10-07 13:23:41 -0400

    first commit

---
+++ HackRU23Back/src/main/java/HackRU23Back/HackRu23BackApplication.java
@@ -0,0 +1,13 @@
+package HackRU23Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackRu23BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackRu23BackApplication.class, args);
+	}
+
+}
---
+++ HackRU23Back/src/test/java/HackRU23Back/HackRu23BackApplicationTests.java
@@ -0,0 +1,13 @@
+package HackRU23Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackRu23BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
