3596c93e9bacd12a33cdf7d174929079c15e2da3
RyamL1221 <ryaml1221@gmail.com>
2023-10-07 14:59:13 -0400

    Added Mongo database

--- HackRU23Back/src/main/java/HackRU23Back/HackRu23BackApplication.java
+++ HackRU23Back/src/main/java/HackRU23Back/HackRu23BackApplication.java
@@ -1,10 +1,14 @@
 package HackRU23Back;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-public class HackRu23BackApplication {
+@EnableMongoRepositories
+public class HackRu23BackApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackRu23BackApplication.class, args);
