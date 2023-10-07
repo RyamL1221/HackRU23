09d2faa3f3672a5aa3fdba4349aabcb830b66415
RyamL1221 <ryaml1221@gmail.com>
2023-10-07 15:35:52 -0400

    commiting

--- HackRU23Back/src/main/java/HackRU23Back/service/StudentService.java
+++ HackRU23Back/src/main/java/HackRU23Back/service/StudentService.java
@@ -1,5 +1,19 @@
 package HackRU23Back.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import HackRU23Back.repo.StudentRepo;
+
+@Service
+@Transactional
 public class StudentService {
     
+    private final StudentRepo studentRepo;
+
+    @Autowired
+    public StudentService(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
 }
