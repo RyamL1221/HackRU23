0c85743cbd82de82da455c41de09b31b216ee61d
brandonbachleda <brandonbachleda@nbp-66-43.nbp.ruw.rutgers.edu>
2023-10-07 16:35:51 -0400

    Brandon Commit 3

--- HackRU23Back/src/main/java/HackRU23Back/model/Course.java
+++ HackRU23Back/src/main/java/HackRU23Back/model/Course.java
@@ -1,10 +1,24 @@
 package HackRU23Back.model;
 
 public class Course {
-	private String name
+	private String name;
 	private String number;
 	private String grade;
 	private int credits;
 	
+	public Course(String n, String num, String g, int c) {
+		name = n;
+		number = num;
+		grade = g;
+		credits = c;
+	}
+	public Course() {
+		name = "";
+		number = "";
+		grade = "";
+		credits = 0;
+	}
+	
+	public
 }
 
