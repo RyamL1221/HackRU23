e5638bdb813242b2c10637bcd9928550f79fe53a
RyamL1221 <ryaml1221@gmail.com>
2023-10-07 15:19:09 -0400

    Database updates

--- HackRU23Back/src/main/java/HackRU23Back/repo/StudentRepo.java
+++ HackRU23Back/src/main/java/HackRU23Back/repo/StudentRepo.java
@@ -1,5 +1,20 @@
 package HackRU23Back.repo;
 
-public interface StudentRepo {
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import HackRU23Back.model.Student;
+
+public interface StudentRepo extends MongoRepository<Student, Long> {
+    
+    @Query("{id:'?0'}")
+    void deleteStudentById(Long id);
+
+    @Query("{id:'?0'}")
+    Student findStudentbyId(Long id);
+
+    public long count();
     
 }
