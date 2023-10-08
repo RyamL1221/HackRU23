df074105a5822b805dc14ee0fb9e0c86fada9269
RyamL1221 <ryaml1221@gmail.com>
2023-10-08 01:04:16 -0400

    started gpa history

--- HackRU23Front/src/app/gpa-history/gpa-history.component.ts
+++ HackRU23Front/src/app/gpa-history/gpa-history.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GpaComponent } from '../gpa/gpa.component';
+import { StudentService } from '../gpa/student.service';
+import { Student } from '../gpa/student';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-gpa-history',
@@ -7,4 +11,22 @@ import { Component } from '@angular/core';
 })
 export class GpaHistoryComponent {
 
+  public students: Student[] = [];
+
+    constructor(private studentService: StudentService) {}
+
+    ngOnInit() {
+      this.getStudents();
+    }
+
+    public getStudents(): void {
+      this.studentService.getStudents().subscribe(
+        (response: Student[]) => {
+          this.students = response;
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      )
+    };
 }
