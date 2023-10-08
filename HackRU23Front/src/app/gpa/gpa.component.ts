018cb65f4a4cfb3c7f376713cbd0b50680ca71fa
RyamL1221 <ryaml1221@gmail.com>
2023-10-07 23:43:30 -0400

    ts

--- HackRU23Front/src/app/gpa/gpa.component.ts
+++ HackRU23Front/src/app/gpa/gpa.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { StudentService } from './student.service';
+import { Student } from './student';
+import { Course } from './course';
+
 
 @Component({
   selector: 'app-gpa',
@@ -7,7 +13,31 @@ import { Component } from '@angular/core';
 })
 export class GpaComponent {
 
+  public students: Student[] = [];
+
+  constructor(private studentService: StudentService) {}
+
+  ngOnInit() {
+    this.getStudents();
+  }
+
+  public getStudents(): void {
+    this.studentService.getStudents().subscribe(
+      (response: Student[]) => {
+        this.students = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    )
+  };
+
+  public counter(i: number) {
+    return new Array(i);
+}
+  public num1 = 0;
   public update(): void{
+    const num = document.getElementById("membership") as HTMLSelectElement
     const courses = []
     courses[0] = document.getElementById("1") as HTMLDivElement
     courses[1] = document.getElementById("2") as HTMLDivElement
@@ -17,7 +47,6 @@ export class GpaComponent {
     courses[5] = document.getElementById("6") as HTMLDivElement
     courses[6] = document.getElementById("7") as HTMLDivElement
     courses[7] = document.getElementById("8") as HTMLDivElement
-    const num = document.getElementById("membership") as HTMLSelectElement
     for (let i = 0; i < courses.length; i++) {
       courses[i].style.display = "block";
     }
