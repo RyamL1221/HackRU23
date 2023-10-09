647b91e64988d62d49d87309e98b50d996b3c982
Depressed Pizza Rolls <basketballcrwm@gmail.com>
2023-10-08 23:04:30 -0400

    added angular material

--- HackRU23Back/src/main/java/HackRU23Back/repo/StudentRepo.java
+++ HackRU23Back/src/main/java/HackRU23Back/repo/StudentRepo.java
@@ -2,10 +2,7 @@ package HackRU23Back.repo;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.stereotype.Repository;
 
 import HackRU23Back.model.Student;
